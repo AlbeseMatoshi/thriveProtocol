94ed508b8251d42d15c091d60300f90cf907edad
albesematoshi <am43526@ubt-uni.net>
2025-01-11 23:49:01 +0100

    Implemented Landing Page & reusable Header and Footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import {Bars3Icon} from "@heroicons/react/24/outline";
+import {Link} from "react-router-dom";
+
+export default function Header(){
+    const navigation = [
+        { name: 'Home', href: '/landing' },
+        { name: 'Dashboard', href: '/dashboard' },
+        { name: 'About', href: '#' },
+    ]
+    return(
+        <header className="absolute inset-x-0 top-0 z-50">
+            <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img
+                            alt=""
+                            src='../../public/thriveprotocol_logo.jpeg'
+                            className="h-8 w-auto"
+                        />
+                    </a>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="size-6" />
+                    </button>
+                </div>
+                <div className="hidden lg:flex lg:gap-x-12">
+                    {navigation.map((item) => (
+                        <Link key={item.name} to={item.href} className="text-sm/6 font-semibold text-gray-900">
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                </div>
+            </nav>
+        </header>
+    )
+}
