b631a06ab19555745c372ccd5e262bf4d0170b4f
albesematoshi <am43526@ubt-uni.net>
2025-01-12 01:35:44 +0100

    Implemented the Layout component to provide a consistent structure with a sidebar and dynamic main content

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,159 @@
+'use client'
+import {Link} from "react-router-dom";
+import { useState } from 'react'
+import {
+    Dialog,
+    DialogBackdrop,
+    DialogPanel,
+    TransitionChild,
+} from '@headlessui/react'
+import {
+    CalendarIcon,
+    ChartPieIcon,
+    Cog6ToothIcon,
+    DocumentDuplicateIcon,
+    FolderIcon,
+    HomeIcon,
+    UsersIcon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+import {ArrowLeftCircleIcon} from "@heroicons/react/16/solid";
+
+const navigation = [
+    { name: 'Dashboard', href: '/dashboard', icon: HomeIcon, current: true },
+    { name: 'Team', href: '/team', icon: UsersIcon, current: false },
+    { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+    { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+    { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
+    { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+]
+const teams = [
+    { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
+    { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
+    { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+]
+
+function classNames(...classes:any) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Sidebar() {
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
+    return (
+        <>
+            <Dialog open={sidebarOpen} onClose={setSidebarOpen} className="relative z-50 lg:hidden">
+                <DialogBackdrop
+                    transition
+                    className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+                />
+
+                <div className="fixed inset-0 flex">
+                    <DialogPanel
+                        transition
+                        className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
+                    >
+                        <TransitionChild>
+                            <div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
+                                <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
+                                    <span className="sr-only">Close sidebar</span>
+                                    <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                                </button>
+                            </div>
+                        </TransitionChild>
+                        {/* Sidebar component, swap this element with another sidebar if you like */}
+                        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
+                            <div className="flex h-16 shrink-0 items-center">
+                                <img
+                                    alt="Your Company"
+                                    src="../../public/thriveprotocol_logo.jpeg"
+                                    className="h-8 w-auto"
+                                />
+                            </div>
+                            <nav className="flex flex-1 flex-col">
+                                <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                    <li>
+                                        <ul role="list" className="-mx-2 space-y-1">
+                                            {navigation.map((item) => (
+                                                <li key={item.name}>
+                                                    <Link
+                                                        to={item.href}
+                                                        className={classNames(
+                                                            item.current
+                                                                ? 'bg-gray-800 text-white'
+                                                                : 'text-gray-400 hover:bg-gray-800 hover:text-white',
+                                                            'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                                                        )}
+                                                    >
+                                                        <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                                                        {item.name}
+                                                    </Link>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </li>
+                                    <Link to={'/landing'} className={'text-white'}>helo</Link>
+                                    <li className="mt-auto">
+                                        <Link
+                                            to={'/landing'}
+                                            className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-400 hover:bg-gray-800 hover:text-white"
+                                        >
+                                            <Cog6ToothIcon aria-hidden="true" className="size-6 shrink-0" />
+                                            Exit Dashboardd
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </DialogPanel>
+                </div>
+            </Dialog>
+                            <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+                    {/* Sidebar component, swap this element with another sidebar if you like */}
+                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
+                        <div className="flex h-16 shrink-0 items-center">
+                            <img
+                                alt="Your Company"
+                                src="../../public/thriveprotocol_logo.jpeg"
+                                className="h-8 w-auto"
+                            />
+                        </div>
+                        <nav className="flex flex-1 flex-col">
+                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                <li>
+                                    <ul role="list" className="-mx-2 space-y-1">
+                                        {navigation.map((item) => (
+                                            <li key={item.name}>
+                                                <Link
+                                                    to={item.href}
+                                                    className={classNames(
+                                                        item.current
+                                                            ? 'bg-gray-800 text-white'
+                                                            : 'text-gray-400 hover:bg-gray-800 hover:text-white',
+                                                        'group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold',
+                                                    )}
+                                                >
+                                                    <item.icon aria-hidden="true" className="size-6 shrink-0" />
+                                                    {item.name}
+                                                </Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                                <li className="mt-auto">
+                                    <Link
+                                        to={'/landing'}
+                                        className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-400 hover:bg-gray-800 hover:text-white"
+                                    >
+                                        <ArrowLeftCircleIcon aria-hidden="true" className="size-6 shrink-0" />
+                                        Exit Dashboard
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+        </>
+    )
+}
